da3add075b8bb67ed6727140e18729a45c2ae957
Richard Brown <33289025+rijobro@users.noreply.github.com>
2022-07-04 13:25:27 +0100

    finish segmentation

--- utils/2d_slice_creator.py
+++ utils/2d_slice_creator.py
@@ -23,39 +23,57 @@ from monai.apps.datasets import DecathlonDataset
 from monai.apps.utils import download_and_extract
 from monai.utils.enums import PostFix
 
+class Slicer(mt.MapTransform):
+    @staticmethod
+    def get_num_non_zero_voxels(im):
+        """Gets the number of non zero voxels along the last dimension. If input image
+        has 100 voxels in z-dimension, then returned will be a 100-element array with number
+        of non-zero voxels in that dimension."""
+        return (im > 0).reshape(-1, im.shape[-1]).sum(0)
 
-class SliceWithMaxNumLabelsd(mt.MapTransform):
+    def get_slice(self, _):
+        raise NotImplementedError()
+
+    def __call__(self, data):
+        _slice = self.get_slice(data)
+        cropper = mt.SpatialCropd(self.keys, roi_slices=[slice(None), slice(None), slice(_slice,_slice+1)])
+        out = cropper(data)
+        for k in self.keys:
+            out[k] = out[k].squeeze(-1)
+        return out
+
+
+class SliceWithMaxNumLabelsd(Slicer):
     """Get a 2D slice of a 3D volume with the maximum number of non-zero voxels in the label."""
 
-    def __init__(self, keys, label_key):
+    def __init__(self, keys, label_key="label"):
         self.keys = keys
         self.label_key = label_key
 
-    def __call__(self, data):
-        d = dict(data)
-        im = d[self.label_key]
-        q = (im > 0).reshape(-1, im.shape[-1]).sum(0)
-        _slice = q.argmax(0)
-        for key in self.keys:
-            d[key] = d[key][..., _slice]
-        return d
+    def get_slice(self, data):
+        num_non_zero_lbl = self.get_num_non_zero_voxels(data[self.label_key])
+        return num_non_zero_lbl.argmax(0)
 
 
-class SaveSliced(mt.MapTransform):
-    """Save the 2D slice to file."""
-
-    def __init__(self, keys, path):
+class SliceWithNoLabelsd(Slicer):
+    """
+    Get a 2D slice of a 3D volume that contains only background class (0) in the segmentation.
+    Of all the 2D slices that contain only zeros in the segmentation, pick the slice that has the
+    largest number of non-zero voxels in the image. This ensures that we have an image that contains
+    lots of healthy tissue.
+    """
+    def __init__(self, keys, label_key="label", image_key="image"):
         self.keys = keys
-        self.path = path
+        self.label_key = label_key
+        self.image_key = image_key
 
-    def __call__(self, data):
-        d = {}
-        for key in self.keys:
-            fname = os.path.basename(data[PostFix.meta(key)]["filename_or_obj"])
-            path = os.path.join(self.path, key, fname)
-            nib.save(nib.Nifti1Image(np.asarray(data[key]), np.eye(4)), path)
-            d[key] = path
-        return d
+    def get_slice(self, data):
+        num_non_zero_img = self.get_num_non_zero_voxels(data[self.image_key])
+        num_non_zero_lbl = self.get_num_non_zero_voxels(data[self.label_key])
+        # ignore any points where the label is non-zero
+        num_non_zero_img[num_non_zero_lbl != 0] = 0
+        # get slice with most non-zero voxels
+        return num_non_zero_img.argmax(0)
 
 
 def download_data(task, download_path):
@@ -73,24 +91,30 @@ def download_data(task, download_path):
 
 def main(task, path, download_path):
     data_dicts = download_data(task, download_path)
-    keys = list(data_dicts[0].keys())
-    for key in keys:
-        os.makedirs(os.path.join(path, key), exist_ok=True)
 
     # list of transforms to convert to 2d slice
     transform_2d_slice = mt.Compose(
         [
-            mt.LoadImaged(keys),
+            mt.LoadImaged(["image", "label"]),
             mt.AsChannelFirstd("image"),
             mt.AddChanneld("label"),
-            SliceWithMaxNumLabelsd(keys, "label"),
-            SaveSliced(keys, path),
+            mt.CopyItemsd(
+                ["image", "label", PostFix.meta("image"), PostFix.meta("label")],
+                times=2,
+                names=["image_tumour", "label_tumour", PostFix.meta("image_tumour"), PostFix.meta("label_tumour"),
+                       "image_healthy", "label_healthy", PostFix.meta("image_healthy"), PostFix.meta("label_healthy")],
+            ),
+            SliceWithMaxNumLabelsd(["image_tumour", "label_tumour"]),
+            SliceWithNoLabelsd(["image_healthy"]),
+            mt.SaveImaged("image_tumour", output_dir=os.path.join(path, "image_tumour"), output_postfix="", separate_folder=False, resample=False, print_log=False),
+            mt.SaveImaged("label_tumour", output_dir=os.path.join(path, "label_tumour"), output_postfix="", separate_folder=False, resample=False, print_log=False),
+            mt.SaveImaged("image_healthy", output_dir=os.path.join(path, "image_healthy"), output_postfix="", separate_folder=False, resample=False, print_log=False),
         ]
     )
 
     for data in tqdm(data_dicts):
         # skip the 2d extraction if possible
-        if len(glob(os.path.join(path, "*", os.path.basename(data["image"])))) == 2:
+        if len(glob(os.path.join(path, "*", os.path.basename(data["image"])))) == 3:
             continue
         # extract the slice
         _ = transform_2d_slice(data)
